2fea6e18845aa81b8a892c7433d23d279c5e624e
Cogin <cevinmontgomery05@gmail.com>
2023-04-10 11:38:15 -0500

    sorting

--- StringListSort.java
+++ StringListSort.java
@@ -1,12 +1,49 @@
 import java.util.ArrayList;
 
-public class StringListSearch {
+public class StringListSort {
 
-    public static int unorderedSearch(ArrayList<String> items, String term) {
-        return 0;
+    public static void SelectionSort( ArrayList<String> list ){
+        String temp;
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i).compareTo(list.get(i-1)) > 0) {
+                for (int j = i+1; j < list.size(); j++) {
+                    if (list.get(i).compareTo(list.get(j)) > 0) {
+                        System.out.println(list.toString());
+                        temp = list.get(i);
+                        list.set(i, list.get(j));
+                        list.set(j, temp);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void InsertionSort( ArrayList<String> list ) {
+        String temp;
+        for (int i = 1; i < list.size(); i++) {
+            int j = i;
+            while (j > 0 && list.get(j).compareTo(list.get(j-1)) < 0) {
+                String tmp = list.get(j-1);
+                list.set(j-1, list.get(j));
+                list.set(j, tmp);
+                j--;
+            };
+        }
     }
     
     public static void main(String[] args) {
-        
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.add("g");
+        list.add("h");
+        list.add("f");
+        list.add("e");
+        list.add("i");
+        list.add("j");
+        SelectionSort(list);
+        System.out.println(list.toString());
     }
 }
--- StringListSortTest.java
+++ StringListSortTest.java
@@ -8,6 +8,14 @@ import org.junit.Test;
 public class StringListSortTest {
     @Test
     public void testSelectionSort() {
-        
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList("a", "b", "c", "d", "g", "f", "e", "h", "i", "j"));
+        StringListSort.SelectionSort(list);
+        assertEquals(list, new ArrayList<String>(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j")));
+    }
+    @Test
+    public void testInsertionSort() {
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList("b", "a", "c", "d", "g", "f", "e", "h", "i", "j"));
+        StringListSort.InsertionSort(list);
+        assertEquals(list, new ArrayList<String>(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j")));
     }
 }
